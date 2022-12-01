ca82a0b6016f65db651a0059fc0bf3b306f902f8
pboros <pboros@tigrisdata.com>
2022-12-01 19:27:22 +0000

    feat: add fdb support to the runner

--- run.sh
+++ run.sh
@@ -30,6 +30,9 @@ RUNTHREADCONF=${RUNTHREADCONF:-"1 2 4 8 16 32 64"}
 RUNTHREADDURATION=${RUNTHREADDURATION:-"1h"}
 RUNTHREADSLEEPINTERVAL=${RUNTHREADSLEEPINTERVAL:-30}
 DROPANDLOAD=${DROPANDLOAD:-0}
+ENGINE=${ENGINE:-"tigris"}
+FDB_CLUSTER_FILE=${FDB_CLUSTER_FILE:-"/mnt/fdb-config-volume/cluster-file"}
+FDB_API_VERSION=${FDB_API_VERSION:-710}
 
 WORKLOAD="recordcount=${RECORDCOUNT}
 operationcount=${OPERATIONCOUNT}
@@ -44,47 +47,106 @@ insertproportion=${INSERTPROPORTION}
 
 requestdistribution=${REQUESTDISTRIBUTION}"
 
+function benchmark_tigris() {
+	# Check if the client works
+	echo "Checking if tigris client is ok"
+
+	${CLI_PATH}tigris ping
+	if [ $? -ne 0 ]
+	then
+		echo "Tigris client has problems, will exit in 30 sec"
+		sleep 30
+		exit 1
+	fi
+
+	echo "Using TIGRIS_HOST ${TIGRIS_HOST}"
+	echo "Using TIGRIS_PORT ${TIGRIS_PORT}"
+	echo "Using TEST_DB ${TEST_DB}"
+	echo "Using LOADTHREADCOUNT ${LOADTHREADCOUNT}"
+
+	if [ "${DROPANDLOAD}" -gt 0 ]
+	then
+		echo "Dropping test database"
+		${CLI_PATH}tigris drop database $TEST_DB
+		sleep 10
+		echo "Loading new database"
+			${BIN_PATH}/go-ycsb load tigris -p tigris.host="$TIGRIS_HOST" -p tigris.port="$TIGRIS_PORT" -p tigris.dbname="$TEST_DB" -P workloads/dynamic -p threadcount=${LOADTHREADCOUNT}
+	fi
+
+	if [ "x${RUNMODE}" == "xsingle" ]
+	then
+		while true
+		do
+			echo "Running benchmark"
+				${BIN_PATH}/go-ycsb run tigris -p tigris.host="$TIGRIS_HOST" -p tigris.port="$TIGRIS_PORT" -p tigris.dbname="$TEST_DB" -P workloads/dynamic -p threadcount=${RUNTHREADCOUNT}
+			echo "Run completed, sleeping before running again"
+			sleep 20
+		done
+	elif [ "x${RUNMODE}" == "xmultiple_threads" ]
+	then
+		while true
+		do
+			for th in ${RUNTHREADCONF}
+			do
+				echo "Running benchmark for ${th} thread(s)"
+				timeout ${RUNTHREADDURATION} ${BIN_PATH}/go-ycsb run tigris -p tigris.host="$TIGRIS_HOST" -p tigris.port="$TIGRIS_PORT" -p tigris.dbname="$TEST_DB" -P workloads/dynamic -p threadcount=${th}
+				sleep ${RUNTHREADSLEEPINTERVAL}
+			done
+		done
+	else
+		echo "Invalid value in RUNMODE variable. Choose between single and multiple_threads."
+	fi
+}
+
+function benchmark_fdb() {
+	echo "Using FDB_CLUSTER_FILE ${FDB_CLUSTER_FILE}"
+	echo "Using FDB_API_VERSION ${FDB_API_VERSION}"
+	echo "Using LOADTHREADCOUNT ${LOADTHREADCOUNT}"
+
+	if [ ${DROPANDLOAD} -gt 0 ]
+	then
+		echo "Loading new database"
+		${BIN_PATH}/go-ycsb load foundationdb -p fdb.clusterfile="${FDB_CLUSTER_FILE}" -p fdb.apiversion="${FDB_API_VERSION}" -P workloads/dynamic -p threadcount=${LOADTHREADCOUNT}
+	fi
+	if [ "x${RUNMODE}" == "xsingle" ]
+	then
+		while true
+		do
+			echo "Running benchmark"
+				${BIN_PATH}/go-ycsb run foundationdb -p fdb.clusterfile="${FDB_CLUSTER_FILE}" -p fdb.apiversion="${FDB_API_VERSION}" -P workloads/dynamic -p threadcount=${RUNTHREADCOUNT}
+			echo "Run completed, sleeping before running again"
+			sleep 20
+		done
+	elif [ "x${RUNMODE}" == "xmultiple_threads" ]
+	then
+		while true
+		do
+			for th in ${RUNTHREADCONF}
+			do
+				echo "Running benchmark for ${th} thread(s)"
+				timeout ${RUNTHREADDURATION} ${BIN_PATH}/go-ycsb run foundationdb -p fdb.clusterfile="${FDB_CLUSTER_FILE}" -p fdb.apiversion="${FDB_API_VERSION}" -P workloads/dynamic -p threadcount=${th}
+				sleep ${RUNTHREADSLEEPINTERVAL}
+			done
+		done
+	else
+		echo "Invalid value in RUNMODE variable. Choose between single and multiple_threads."
+	fi
+}
+
 echo "${WORKLOAD}" > workloads/dynamic
 
-# Check if the client works
-echo "Checking if tigris client is ok"
-${CLI_PATH}tigris list databases
-if [ $? -ne 0 ]
-then
-	echo "Tigris client has problems, will exit in 30 sec"
-	sleep 30
-	exit 1
-fi
+echo "Using engine ${ENGINE}"
 
-if [ ${DROPANDLOAD} -gt 0 ]
-then
-	echo "Dropping test database"
-	${CLI_PATH}tigris drop database $TEST_DB
-	sleep 10
-	echo "Loading new database"
-		${BIN_PATH}/go-ycsb load tigris -p tigris.host="$TIGRIS_HOST" -p tigris.port="$TIGRIS_PORT" -p tigris.dbname="$TEST_DB" -P workloads/dynamic -p threadcount=${LOADTHREADCOUNT}
-fi
+case ${ENGINE} in
+	"tigris")
+		benchmark_tigris
+	;;
+  "foundationdb")
+  	benchmark_fdb
+  ;;
+  *)
+  	echo "Unknown engine"
+  	exit 1
+  	;;
+esac
 
-if [ "x${RUNMODE}" == "xsingle" ]
-then
-	while true
-	do
-		echo "Running benchmark"
-			${BIN_PATH}/go-ycsb run tigris -p tigris.host="$TIGRIS_HOST" -p tigris.port="$TIGRIS_PORT" -p tigris.dbname="$TEST_DB" -P workloads/dynamic -p threadcount=${RUNTHREADCOUNT}
-		echo "Run completed, sleeping before running again"
-		sleep 20
-	done
-elif [ "x${RUNMODE}" == "xmultiple_threads" ]
-then
-	while true
-	do
-		for th in ${RUNTHREADCONF}
-		do
-			echo "Running benchmark for ${th} thread(s)"
-			timeout ${RUNTHREADDURATION} ${BIN_PATH}/go-ycsb run tigris -p tigris.host="$TIGRIS_HOST" -p tigris.port="$TIGRIS_PORT" -p tigris.dbname="$TEST_DB" -P workloads/dynamic -p threadcount=${th}
-			sleep ${RUNTHREADSLEEPINTERVAL}
-		done
-	done
-else
-	echo "Invalid value in RUNMODE variable. Choose between single and multiple_threads."
-fi
